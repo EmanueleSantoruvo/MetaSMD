66e154b17e2522fe0bf1f0f372bccdfa4a06a689
StefaniaDimundo <151828985+StefaniaDimundo@users.noreply.github.com>
2024-05-18 11:03:43 +0200

    Marengo

--- src/main/java/com/example/metasmd/MyServlet.java
+++ src/main/java/com/example/metasmd/MyServlet.java
@@ -39,6 +39,6 @@ public class MyServlet extends HttpServlet {
      */
     protected void doPost(HttpRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        
+
     }
 }
